38b19f27c5b26355065d962bc7f1bfd9c154c2e4
Ernesto J. Suárez Pons <ejsp2005@gmail.com>
2022-09-08 19:39:17 -0400

    actors and gender included. lite changes in layout

--- models/film_list.py
+++ models/film_list.py
@@ -6,8 +6,23 @@ class FilmList(models.Model):
     _name = 'film.list'
     _description = 'This is a list of films.'
 
-    name = fields.Char(required=True)
-    description = fields.Text()
-    # year = fields.Char()
-    # director = fields.Char()
+    name = fields.Char(required=True, copy=False)
+    description = fields.Text(copy=False)
+    year = fields.Char(required=True, copy=False)
+    director = fields.Char(required=True, copy=False)
+    actor = fields.Char(copy=False)
+    actress = fields.Char(copy=False)
     tags_ids = fields.Many2many("film.list.tag")
+    gender = fields.Selection(
+        selection=[
+            ('scifi', 'Science Fiction'),
+            ('adventure', 'Adventure'),
+            ('action', 'Action'),
+            ('animation', 'Animation'),
+            ('war', 'War'),
+            ('thriller', 'Thriller'),
+            ('romance', 'Romance'),
+            ('horror', 'Horror'),
+            ('comedy', 'Comedy')
+        ]
+    )
